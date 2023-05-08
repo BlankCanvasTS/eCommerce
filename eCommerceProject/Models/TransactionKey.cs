1ccbc033d2799bb4874e10e3bc137fc7a1c69139
BlankCanvasTS <93033104+BlankCanvasTS@users.noreply.github.com>
2023-05-07 17:05:23 -0700

    Transaction Key Class

---
+++ eCommerceProject/Models/TransactionKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace eCommerceProject.Models
+{
+    public class TransactionKey
+    {
+        public static string CreateTransactionKey(string transactionId, DateTime timestamp, decimal amount)
+    {
+        // Combine the transaction data into a single string
+        string transactionData = $"{transactionId}|{timestamp.ToString("yyyyMMddHHmmssfff")}|{amount.ToString("F2")}";
+
+        // Compute the hash value using the SHA256 algorithm
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(transactionData));
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+    }
+}
+}
