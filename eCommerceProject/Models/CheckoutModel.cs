23819735f38d63b5f7736fd083eb546c03f5890d
Abelc-22 <65688483+Abelc-22@users.noreply.github.com>
2023-05-10 13:20:47 -0700

    Checkout css (#3)

---
+++ eCommerceProject/Models/CheckoutModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceProject.Models
+{
+    public class CheckoutModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "CardNumber")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Display(Name = "CardExpDate")]
+        public string CardExpDate { get; set; }
+
+        [Required]
+        [Display(Name = "CardSecCode")]
+        public string? CardSecCode { get; set; }
+        public int CardFunds { get; set; }
+        public Boolean OnHold { get; set; }
+
+
+    }
+}
--- eCommerceProject/Models/TransactionKey.cs
+++ eCommerceProject/Models/TransactionKey.cs
@@ -6,10 +6,10 @@ namespace eCommerceProject.Models
 {
     public class TransactionKey
     {
-        public static string CreateTransactionKey(string transactionId, DateTime timestamp, decimal amount)
+        public static string CreateTransactionKey(string transactionId, DateTime timestamp)
     {
         // Combine the transaction data into a single string
-        string transactionData = $"{transactionId}|{timestamp.ToString("yyyyMMddHHmmssfff")}|{amount.ToString("F2")}";
+        string transactionData = $"{transactionId}|{timestamp.ToString("yyyyMMddHHmmssfff")}";
 
         // Compute the hash value using the SHA256 algorithm
         using (SHA256 sha256 = SHA256.Create())
